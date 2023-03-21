5b89c27ae2c079a202f880a6f98ede5221839dff
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2023-03-21 13:33:54 -0500

    fix bug in SplitPeptides

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
 using MS1_DataSimulator;
 
 List<string> fullPathToFastas = new List<string>() { @"C:\Users\mrsho\Documents\Projects\MSDataSimulator\oneProtein.fasta" };
-BuildAnMzmlChromatogram chromatogram = new (fullPathToFastas, 0.9);
+BuildAnMzmlChromatogram chromatogram = new (fullPathToFastas, 0.5);
 
 Console.WriteLine("Hello, World!");
