3d919eec1c2761d193b0c22786ae2345bfa26e15
MICHAEL SHORTREED <mrshortreed@wisc.edu>
2023-03-21 12:59:56 -0500

    proteasesTsv added

--- DigestProteinsIntoPeptides.cs
+++ DigestProteinsIntoPeptides.cs
@@ -1,29 +1,30 @@
-﻿using Proteomics;
+﻿using Chemistry;
+using Proteomics;
 using Proteomics.ProteolyticDigestion;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UsefulProteomicsDatabases;
 
 namespace MS1_DataSimulator
 {
     internal class DigestProteinsIntoPeptides
     {
+        public static string? DataDir { get; private set; }
         public readonly PeptideWithSetModifications[] Peptides;
 
         public DigestProteinsIntoPeptides(List<string> fullPathsToFastaFiles, int minPeptideLength = 7, int maxMissedCleavages = 0)
         {
+            
+            Loaders.LoadElements();
+            DataDir = SetUpDataDirectory();
+            var protDic = ProteaseDictionary.LoadProteaseDictionary(Path.Combine(DataDir, @"ProteolyticDigestion", @"proteases.tsv"));
+
             List<PeptideWithSetModifications> peptides = new();
             foreach (string path in fullPathsToFastaFiles)
             {
                 if (File.Exists(path))
                 {
-                    List<Protein> proteins = ProteinDbLoader.LoadProteinFasta(path, true, DecoyType.None,
-                        false, out var errors, ProteinDbLoader.UniprotAccessionRegex, ProteinDbLoader.UniprotFullNameRegex, ProteinDbLoader.UniprotNameRegex, ProteinDbLoader.UniprotGeneNameRegex,
-                        ProteinDbLoader.UniprotOrganismRegex);
-                    foreach (Protein protein in proteins)
+                    List<Protein> proteinList = ProteinDbLoader.LoadProteinFasta(path, true, DecoyType.None, false, out List<string> errors);
+
+                    foreach (Protein protein in proteinList)
                     {
                         peptides.AddRange(protein.Digest(new DigestionParams(minPeptideLength: minPeptideLength, maxMissedCleavages: maxMissedCleavages), new List<Modification>(), new List<Modification>()).ToList());
                     }
@@ -32,6 +33,20 @@ namespace MS1_DataSimulator
 
             Peptides = peptides.ToArray();
             peptides.Clear();
+        }
+        private static string SetUpDataDirectory()
+        {
+            // get data directory
+            var pathToProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            if (!String.IsNullOrWhiteSpace(pathToProgramFiles) && AppDomain.CurrentDomain.BaseDirectory.Contains(pathToProgramFiles)
+                && !AppDomain.CurrentDomain.BaseDirectory.Contains("Jenkins"))
+            {
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MetaMorpheus");
+            }
+            else
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 using MS1_DataSimulator;
 
-List<string> fullPathToFastas = new List<string>() { @"E:\junk\Downloads_Edrive\yeast.fasta" };
-BuildAChromatogram chromatogram = new (fullPathToFastas, 0.9);
+List<string> fullPathToFastas = new List<string>() { @"C:\Users\mrsho\Documents\Projects\MSDataSimulator\oneProtein.fasta" };
+BuildAnMzmlChromatogram chromatogram = new (fullPathToFastas, 0.9);
 
 Console.WriteLine("Hello, World!");
